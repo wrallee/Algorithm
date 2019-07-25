799059b10427ae64c61ed3fd4ddd79cc9567b0c1
wrallee <wrallee@naver.com>
2019-07-26 00:27:39 +0900

    BOJ2261/5620: Closest Set

--- src/ClosestSet2_BOJ5620.java
+++ src/ClosestSet2_BOJ5620.java
@@ -1,14 +1,13 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.SortedSet;
+import java.util.Arrays;
+import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.TreeSet;
 
 public class ClosestSet2_BOJ5620 {
 	static int N;
-	static ArrayList<Point> list;
+	static Point[] list;
 	static TreeSet<Point> candidates;
 	public static void main(String[] args) throws Exception {
 		//System.setIn(new  FileInputStream("TC.txt"));
@@ -16,27 +15,27 @@ public class ClosestSet2_BOJ5620 {
 		StringTokenizer st;
 		
 		N = Integer.parseInt(br.readLine());
-		list = new ArrayList<>();
+		list = new Point[N];
 		
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
-			list.add(new Point(x, y));
+			list[i] = new Point(x, y);
 		}
-		Collections.sort(list, (o1, o2) -> o1.x - o2.x); // Sort Asc
+		Arrays.sort(list, (o1, o2) -> o1.x - o2.x); // Sort Asc
 		candidates = new TreeSet<>((o1, o2) -> (o1.y != o2.y ? o1.y - o2.y : o1.x - o2.x));
-		candidates.add(list.get(0));
-		candidates.add(list.get(1));
+		candidates.add(list[0]);
+		candidates.add(list[1]);
 		
-		int ans = getDistance(list.get(0), list.get(1));
+		int ans = getDistance(list[0], list[1]);
 		int start = 0;
 		for (int i = 2; i < N; i++) {
-			Point now = list.get(i);
+			Point now = list[i];
 			
 			// x축 기준으로 d값 세팅
 			while (start < i) {
-				Point pivot = list.get(start);
+				Point pivot = list[start];
 				int dx = pivot.x - now.x;
 				if (dx*dx > ans) {
 					candidates.remove(pivot);
@@ -45,10 +44,11 @@ public class ClosestSet2_BOJ5620 {
 					break;
 				}
 			}
-			int d = (int) Math.ceil(Math.sqrt(ans));
+			int d = (int) Math.sqrt((double) ans) + 1;
 			
-			Point lowerPoint = new Point(now.y - d, now.y + d);
-			SortedSet<Point> ss = candidates.headSet(lowerPoint);
+			Point lowerBound = new Point(-10001, now.y-d);
+			Point upperBound = new Point(10001, now.y+d);
+			Set<Point> ss = candidates.subSet(lowerBound, upperBound);
 			for (Point point : ss) {
 				int dist = getDistance(now, point);
 				if (ans > dist)
@@ -66,18 +66,15 @@ public class ClosestSet2_BOJ5620 {
 		return (int)(Math.pow(p1.x-p2.x, 2) + Math.pow(p1.y-p2.y, 2));
 	}
 	
-	static class Point implements Comparable<Point> {
+	static class Point {
 		int x, y;
 		public Point(int x, int y) {
 			this.x = x;
 			this.y = y;
 		}
 		@Override
-		public int compareTo(Point o) {
-			// 1: 기준(lowerPoint)이 더 큼
-			//   -> 기준보다 작은 값 확인
-			//   -> Sort Asc + headSet
-			return x < o.y && o.y < y ? 1 : -1;
+		public String toString() {
+			return "["+x+","+y+"]";
 		}
 	}
 }
--- src/ClosestSet_BOJ2261.java
+++ src/ClosestSet_BOJ2261.java
@@ -1,23 +1,21 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Set;
+import java.util.Collections;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
 
 public class ClosestSet_BOJ2261 {
-	static int N;
+	static int N, size;
 	static Point[] list;
-	static TreeSet<Point> candidates;
 	public static void main(String[] args) throws Exception {
-		//System.setIn(new  FileInputStream("TC.txt"));
+		// System.setIn(new FileInputStream("TC.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		
+
 		N = Integer.parseInt(br.readLine());
 		list = new Point[N];
-		
+
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken());
@@ -25,64 +23,62 @@ public class ClosestSet_BOJ2261 {
 			list[i] = new Point(x, y);
 		}
 		Arrays.sort(list, (o1, o2) -> o1.x - o2.x); // Sort Asc
-		candidates = new TreeSet<>((o1, o2) -> (o1.y != o2.y ? o1.y - o2.y : o1.x - o2.x));
-		candidates.add(list[0]);
-		candidates.add(list[1]);
-		
-		int ans = getDistance(list[0], list[1]);
-		int start = 0;
-		for (int i = 2; i < N; i++) {
-			Point now = list[i];
-			
-			// x축 기준으로 d값 세팅
-			while (start < i) {
-				Point pivot = list[start];
-				int dx = pivot.x - now.x;
-				if (dx*dx > ans) {
-					candidates.remove(pivot);
-					start++;
-				} else {
-					break;
-				}
-			}
-			int d = (int) Math.sqrt((double) ans) + 1;
-			
-			Point lowerBound = new Point(-10001, now.y-d);
-			Point upperBound = new Point(10001, now.y+d);
-			Set<Point> ss = candidates.subSet(lowerBound, upperBound);
-			for (Point point : ss) {
-				int dist = getDistance(now, point);
-				if (ans > dist)
-					ans = dist;
-			}
-			candidates.add(now);
-		}
-		
-		System.out.println(ans);
-		
+
+		System.out.println(closestSet(0, N - 1));
+		// System.out.println(System.currentTimeMillis() - stt);
+
 		br.close();
 	}
-	
-	static int getDistance(Point p1, Point p2) {
-		return (int)(Math.pow(p1.x-p2.x, 2) + Math.pow(p1.y-p2.y, 2));
+
+	static int closestSet(int stt, int end) {
+		if (end == stt)
+			return 987654321;
+
+		int mid = (stt + end) / 2;
+		int left = closestSet(stt, mid);
+		int right = closestSet(mid + 1, end);
+		int min = Math.min(left, right);
+
+		ArrayList<Point> l = new ArrayList<>();
+
+		for (int i = stt; i <= end; i++) {
+			int dx = list[i].x - list[mid].x;
+			if (dx * dx < min)
+				l.add(list[i]);
+		}
+
+		Collections.sort(l);
+
+		for (int i = 0; i < l.size() - 1; i++) {
+			Point prev = l.get(i);
+			for (int j = i + 1; j < l.size(); j++) {
+				Point next = l.get(j);
+				int dy = prev.y - next.y;
+				if (dy * dy > min)
+					break;
+
+				min = Math.min(min, getDistance(prev, next));
+			}
+		}
+
+		return min;
 	}
-	
+
+	static int getDistance(Point p1, Point p2) {
+		return (int) (Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2));
+	}
+
 	static class Point implements Comparable<Point> {
 		int x, y;
+
 		public Point(int x, int y) {
 			this.x = x;
 			this.y = y;
 		}
+
 		@Override
 		public int compareTo(Point o) {
-			// 1: 기준(lowerPoint)이 더 큼
-			//   -> 기준보다 작은 값 확인
-			//   -> Sort Asc + headSet
-			return x < o.y && o.y < y ? 1 : -1;
-		}
-		@Override
-		public String toString() {
-			return "["+x+","+y+"]";
+			return this.y - o.y;
 		}
 	}
 }
